09a3e86249c786a79a822be9d0c85ea63ac9de1c
Arnold KOUAKEP <kouakeparnold27@gmail.com>
2022-10-19 04:01:29 +0100

    correction bug sur la nomenclature du nom du package de test.

--- pricer/src/test/java/fr/ingeniance/kata/unitconversionpricer/TestUnitConversionPricer.java
+++ pricer/src/test/java/fr/ingeniance/kata/unitconversionpricer/TestUnitConversionPricer.java
@@ -1,4 +1,4 @@
-package fr.ingeniance.kata.unitConverti;
+package fr.ingeniance.kata.unitconversionpricer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
