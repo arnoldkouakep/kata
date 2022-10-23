7638be96b906ab4c2d78eb014ac576c82223c8eb
Arnold KOUAKEP <kouakeparnold27@gmail.com>
2022-10-23 23:48:56 +0200

    Correction du bug maven test sur le module roman : rendre les classes et methodes public.

--- roman/src/test/java/fr/ingeniance/kata/roman/TestRoman.java
+++ roman/src/test/java/fr/ingeniance/kata/roman/TestRoman.java
@@ -7,10 +7,10 @@ import org.junit.Test;
 
 import fr.ingeniance.kata.roman.exception.NotRomanException;
 
-class TestRoman {
+public class TestRoman {
 
 	@Test
-	void whenExceptionThrown_thenNumberIsNegative() throws Exception {
+	public void whenExceptionThrown_thenNumberIsNegative() throws Exception {
 
 		NotRomanException ex = assertThrows(NotRomanException.class, () -> {
 			Roman.convertToRoman(-1);
@@ -21,22 +21,22 @@ class TestRoman {
 	}
 
 	@Test
-	void printRomanTest1() throws Exception {
+	public void printRomanTest1() throws Exception {
 		assertEquals("I", Roman.convertToRoman(1));
 	}
 
 	@Test
-	void printRomanTest4() throws Exception {
+	public void printRomanTest4() throws Exception {
 		assertEquals("IV", Roman.convertToRoman(4));
 	}
 
 	@Test
-	void printRomanTest1954() throws Exception {
+	public void printRomanTest1954() throws Exception {
 		assertEquals("MCMLIV", Roman.convertToRoman(1954));
 	}
 
 	@Test
-	void printRomanTest1990() throws Exception {
+	public void printRomanTest1990() throws Exception {
 		assertEquals("MCMXC", Roman.convertToRoman(1990));
 	}
 
